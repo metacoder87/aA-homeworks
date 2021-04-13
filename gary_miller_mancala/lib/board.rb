d162dd64c2cb700f37b5f4c87a0522751f96854d
metacoder87 <gary.miller@gmx.com>
2021-04-13 10:26:34 -0400

    make_move still wip

--- gary_miller_mancala/lib/board.rb
+++ gary_miller_mancala/lib/board.rb
@@ -1,14 +1,17 @@
+require 'byebug'
+
 class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
     @name1, @name2 = name1, name2
     @cups = Array.new(14, [])
-    @cups.map!.with_index { |cup, i| i == 6 || i == 13 ? cup : cup = [:stone, :stone, :stone, :stone] }
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.map!.with_index { |cup, i| i == 6 || i == 13 ? cup : cup = [:stone, :stone, :stone, :stone] }
   end
 
   def valid_move?(start_pos)
@@ -27,14 +30,16 @@ class Board
     @cups[start_pos].each { |stone| stones_in_hand << stone }
     @cups[start_pos] = []
     # distributes the stones from that cup
-    until stones_in_hand.empty?
+    debugger
+    unless stones_in_hand.empty?
       if current_player_name == @name1
-        (start_pos + 1..7).each { |cup| @cups[cup] << stones_in_hand.pop } if start_pos < 6
+        (start_pos + 1..7).each { |cup| @cups[cup] << stones_in_hand.pop } if start_pos < 6 
         (start_pos + 1..12).each { |cup| @cups[cup] << stones_in_hand.pop } if start_pos > 6
       else
         (start_pos + 1..6).each { |cup| @cups[cup] << stones_in_hand.pop } if start_pos < 6
         (start_pos + 1..13).each { |cup| @cups[cup] << stones_in_hand.pop } if start_pos > 6
       end
+      @cups.map! { |cup| cup.compact }
     end
   end
 
