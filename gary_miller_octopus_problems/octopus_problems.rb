e6f39f251ec7d095d879f5abf2202f9864d70ecc
metacoder87 <gary.miller@gmx.com>
2021-10-17 11:51:08 -0400

    Adds sluggish_octopus w/ O(n^2) time

--- gary_miller_octopus_problems/octopus_problems.rb
+++ gary_miller_octopus_problems/octopus_problems.rb
@@ -8,8 +8,11 @@ class Octopus
         @school = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
     end
 
-    def sluggish_octopus
-        @school.each { |craniata| return craniata if @school.all? { |cran| cran.length <= craniata.length } }
+    def sluggish_octopus # O(n^2)
+        @school.each do |craniata| 
+            return craniata if @school.all? { |cran| cran.length <= craniata.length }
+        end
+    end
     end
 
 end
