1f5a36098b302dd3d6513f5d05631de4a9da8780
metacoder87 <gary.miller@gmx.com>
2021-10-17 23:35:00 -0400

    Adds in some comments

--- gary_miller_octopus_problems/octopus_problems.rb
+++ gary_miller_octopus_problems/octopus_problems.rb
@@ -4,12 +4,18 @@ class Octopus
 
 attr_reader :school, :tiles_array, :new_tiles_data_structure
 
-    def initialize
+    def initialize  # Creates the Octopus class with a school of fish, array of tiles and a hash of those tiles
         @school = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
         @tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
         @new_tiles_data_structure = {}
         new_data_filler
     end
+    # A Very Hungry Octopus wants to eat the longest fish in an array of fish.
+
+    # Sluggish Octopus
+
+        # Find the longest fish in O(n^2) time. Do this by comparing all fish 
+        # lengths to all other fish lengths
 
     def sluggish_octopus # O(n^2)
         @school.each do |craniata| 
@@ -17,9 +23,21 @@ attr_reader :school, :tiles_array, :new_tiles_data_structure
         end
     end
 
+    # Dominant Octopus
+        # Find the longest fish in O(n log n) time. Hint: You saw a sorting 
+        # algorithm that runs in O(n log n) in the Sorting Complexity Demo. 
+        # Remember that Big O is classified by the dominant term.
+
     def dominant_octopus # O(n*log(n))
         merge_sort(@school).last
     end
+
+    # Clever Octopus
+
+        # Find the longest fish in O(n) time. 
+        # The octopus can hold on to the longest fish that you have 
+        # found so far while stepping through the array only once.
+
 
     def clever_octopus # O(n)
         biggest_fish = ""
@@ -29,15 +47,38 @@ attr_reader :school, :tiles_array, :new_tiles_data_structure
 
     # DANCING OCTOPUS
 
+        # Full of fish, the Octopus attempts Dance Dance Revolution. 
+        # The game has tiles in the following directions: tiles_array = 
+        # [
+            # "up", "right-up", "right", "right-down", 
+            # "down", "left-down", "left",  "left-up" 
+        # ]
+
+        # To play the game, the octopus must step on a tile with her 
+        # corresponding tentacle. We can assume that the octopus's eight 
+        # tentacles are numbered and correspond to the tile direction indices.
+
+    # Slow Dance
+
+        # Given a tile direction, iterate through a tiles array to return 
+        # the tentacle number (tile index) the octopus must move. 
+        # This should take O(n) time.
+
     def slow_dance(move, tiles) # O(n)
         tiles.each_with_index do |tile, idx| 
             return idx if tile == move
         end
     end
 
+    # Constant Dance!
+
+        # Now that the octopus is warmed up, let's help her dance faster. 
+        # Use a different data structure and write a new function 
+        # so that you can access the tentacle number in O(1) time.
+
     def fast_dance(move, tiles) # O(1)
         return tiles[move]
-    end
+    end 
 
     private
 
@@ -51,10 +92,10 @@ attr_reader :school, :tiles_array, :new_tiles_data_structure
 
         mid_idx = array.length / 2
         merge(
-        merge_sort(array.take(mid_idx), &prc),
-        merge_sort(array.drop(mid_idx), &prc),
-        &prc
-        )
+            merge_sort(array.take(mid_idx), &prc),
+            merge_sort(array.drop(mid_idx), &prc),
+            &prc
+            )
     end
 
     # NB: In Ruby, shift is an O(1) operation. This is not true of all languages.
@@ -62,14 +103,14 @@ attr_reader :school, :tiles_array, :new_tiles_data_structure
         merged_array = []
         prc = Proc.new { |fish1, fish2| fish1.length <=> fish2.length } unless block_given?
         until left.empty? || right.empty?
-        case prc.call(left.first, right.first)
-        when -1
-            merged_array << left.shift
-        when 0
-            merged_array << left.shift
-        when 1
-            merged_array << right.shift
-        end
+            case prc.call(left.first, right.first)
+                when -1
+                    merged_array << left.shift
+                when 0
+                    merged_array << left.shift
+                when 1
+                    merged_array << right.shift
+            end
         end
 
         merged_array + left + right
