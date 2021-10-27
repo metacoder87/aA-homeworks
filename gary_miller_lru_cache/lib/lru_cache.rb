4c6fdea52c39d601ebfd7e2ff3d8260c72b08a4e
metacoder87 <gary.miller@gmx.com>
2021-10-27 10:49:05 -0400

    Answers questions and adds comments

--- gary_miller_lru_cache/lib/lru_cache.rb
+++ gary_miller_lru_cache/lib/lru_cache.rb
@@ -26,11 +26,21 @@ class LRUCache
     # helper methods go here!
 
     def cancel_double?(el)
+        # Checks if the element is already in the Cache and removes it if it is.
         @store.delete(el) if @store.include?(el)
     end
 
     def limit_reached?
+        # Checks if the cache is currently full and removes the oldest element if it is.
         @store.shift if count == @cache_limit
     end
 
 end
+
+# This is not your typical LRU Cache because the time complexity of this 
+# algorithm with primitive Array data type does not scale well with larger
+# cache sizes. My LRUCache#add method is also O(n) in order to search the 
+# for an element before adding that element it must make a linear pass.
+# If as the lesson described we had used a Doubley Linked List and accessed
+# the links through a Hash key, we would be able to cut the add time down
+# to O(1) Constant time amortized. 
