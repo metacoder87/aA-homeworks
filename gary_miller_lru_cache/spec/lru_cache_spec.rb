9bc1640df439168c101ef25dfed409a985ca2ede
metacoder87 <gary.miller@gmx.com>
2021-10-27 10:37:46 -0400

    Test for add

--- gary_miller_lru_cache/spec/lru_cache_spec.rb
+++ gary_miller_lru_cache/spec/lru_cache_spec.rb
@@ -17,3 +17,12 @@ describe LRUCache do
           expect(johnny_cache.count).to eq(0)
       end
   end
+
+  describe 'add' do
+      it 'adds an element to the array' do
+        johnny_cache.add("I walk the line")
+        johnny_cache.add(5)
+          expect(johnny_cache.count).to eq(2)
+      end
+  end
+
