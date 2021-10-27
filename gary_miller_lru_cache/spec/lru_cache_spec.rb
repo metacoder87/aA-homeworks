3e6a76bbc5f4e0d7ec837feeec075e1282160174
metacoder87 <gary.miller@gmx.com>
2021-10-27 10:38:45 -0400

    Adds tests for show and finishes rspec

--- gary_miller_lru_cache/spec/lru_cache_spec.rb
+++ gary_miller_lru_cache/spec/lru_cache_spec.rb
@@ -48,3 +48,19 @@ describe LRUCache do
             expect(johnny_cache.count).to eq(4)
       end
   end
+
+  describe 'show' do
+      it 'prints the contents of the cache' do
+        johnny_cache.add(-5)
+        johnny_cache.add({a: 1, b: 2, c: 3})
+        johnny_cache.add([1,2,3,4])
+        johnny_cache.add("I walk the line")
+        johnny_cache.add(:ring_of_fire)
+        johnny_cache.add("I walk the line")
+          expect { johnny_cache.show }.to output(
+              "[{:a=>1, :b=>2, :c=>3}, [1, 2, 3, 4], :ring_of_fire, \"I walk the line\"]"
+            ).to_stdout
+      end
+  end
+
+end
