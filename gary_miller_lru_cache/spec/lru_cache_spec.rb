d5c26f58984073be664916932b1b50c198e7bb86
metacoder87 <gary.miller@gmx.com>
2021-10-27 10:37:24 -0400

    Tests for initialize

--- gary_miller_lru_cache/spec/lru_cache_spec.rb
+++ gary_miller_lru_cache/spec/lru_cache_spec.rb
@@ -5,3 +5,9 @@ describe LRUCache do
   subject(:johnny_cache) do
     LRUCache.new(4)
   end
+
+  describe 'initialize' do
+      it 'creates an instance of LRUCache' do
+        expect(johnny_cache.class).to eq(LRUCache)
+      end
+  end
