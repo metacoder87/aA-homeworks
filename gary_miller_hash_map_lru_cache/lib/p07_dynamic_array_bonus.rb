075a8fbc319c84b93fedd7fb63c31a33a587cd14
metacoder87 <gary.miller@gmx.com>
2021-11-15 00:19:44 -0500

    Almost finished with dyna_array_bonus

--- gary_miller_hash_map_lru_cache/lib/p07_dynamic_array_bonus.rb
+++ gary_miller_hash_map_lru_cache/lib/p07_dynamic_array_bonus.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class StaticArray
   attr_reader :store
 
@@ -27,6 +29,8 @@ class StaticArray
 end
 
 class DynamicArray
+  include Enumerable
+
   attr_accessor :count
 
   def initialize(capacity = 8)
@@ -35,9 +39,11 @@ class DynamicArray
   end
 
   def [](i)
+    @store.store[i]
   end
 
   def []=(i, val)
+    @store.store[i] = val
   end
 
   def capacity
@@ -45,27 +51,53 @@ class DynamicArray
   end
 
   def include?(val)
+    @store.store.any? { |el| el == val } 
   end
 
   def push(val)
+    resize! if @count == capacity
+    @store[@count] = val
+    @count += 1
   end
 
   def unshift(val)
+    new_val = val
+    @store.store.each.with_index do |ele, idx|
+      @store[idx] = new_val
+      new_val = ele
+    end
+    @count += 1
   end
 
   def pop
+    return nil if first.nil?
+    popped = last
+    @store[@count - 1] = nil
+    @count -= 1
+    return popped
   end
 
   def shift
+    return first if first.nil?
+    shifted = first
+
+    @store.store.each.with_index do |el, idx|
+      @store[idx - 1] = el if idx > 0
+    end
+    @count -= 1
+    return shifted
   end
 
   def first
+    @store[0]
   end
 
   def last
+    @store[@count - 1]
   end
 
   def each
+    @store.store.each { |ele| yield ele }
   end
 
   def to_s
@@ -75,6 +107,7 @@ class DynamicArray
   def ==(other)
     return false unless [Array, DynamicArray].include?(other.class)
     # ...
+    return other == @store.store
   end
 
   alias_method :<<, :push
@@ -83,5 +116,11 @@ class DynamicArray
   private
 
   def resize!
+    new_store = StaticArray.new(capacity)
+    @count = 0
+    self.each do |ele|
+      new_store.store << ele
+    end
+    @store = new_store
   end
 end
