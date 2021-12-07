99c33b0d6f261b9eb2b31bcf347b256eb49e5c54
metacoder87 <gary.miller@gmx.com>
2021-12-07 00:23:28 -0500

    Adds database solutions

--- gary_miller_plays_playwrights_orm/plays.rb
+++ gary_miller_plays_playwrights_orm/plays.rb
@@ -19,6 +19,32 @@ class Play
     data.map { |datum| Play.new(datum) }
   end
 
+  def self.find_by_title(title)
+    play = PlayDBConnection.instance.execute(<<-SQL, self.title)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        title = ?
+    SQL
+    return nil if play.count < 1
+    return Play.new(play.first)
+  end
+
+  def self.find_by_playwright(name)
+    playwright = PlayDBConnection.instance.execute(<<-SQL, self.name )
+      SELECT
+        *
+      FROM
+        playwrights
+      WHERE
+        name = playwright.name
+    SQL
+    return nil if playwright.count < 1
+    return Playwright.new
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
@@ -49,18 +75,67 @@ class Play
     SQL
   end
 
-  def find_by_title(title)
+end
+
+class Playwright
+
+  def self.all
+    data = PlayDBConnection.instance.execute(SELECT * FROM playwrights)
+    data.map { |datum| Playwright.new(datum) }
   end
 
-  def find_by_playwright(name)
+  def self.find_by_name(name)
+    playwright = PlayDBConnection.instance.execute(<<-SQL, self.name )
+      SELECT
+        *
+      FROM
+        playwrights
+      WHERE
+        name = playwright.name
+    SQL
+    return nil if playwright.count < 1
+    return Playwright.new
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL self.name, self.birth_year)
+    INSERT INTO
+      playwrights (name, birth_year)
+    VALUES
+      (?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL self.name, self.birth_year, self.id)
+    UPDATE
+      playwrights
+    SET
+      name = ?, birth_year = ?
+    WHERE
+      id = ?
+    SQL
+  end
+
+  def get_plays(name) # (returns all plays written by playwright)
+    plays = PlayDBConnection.instance.execute(<<-SQL self.name)
+    SELECT
+      *
+    FROM
+      playwrights
+    WHERE
+      name = ?
+    SQL
+      return plays
   end
 
 end
-
-# Playwright::all
-# Playwright::find_by_name(name)
-# Playwright#new (this is the initialize method)
-# Playwright#create
-# Playwright#update
-# Playwright#get_plays (returns all plays written by playwright)
-
